269da6debc086a011b9554b6f95dbc5c57f2cd18
Kelly online <kellyonline2016@gmail.com>
2020-03-24 22:04:32 +0100

    modified comments and values

--- csharp/Tasks/Division/Program.cs
+++ csharp/Tasks/Division/Program.cs
@@ -6,7 +6,38 @@ namespace Division
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declare and initialize the variables
+            int firstnumber = 0;   int secondnumber = 0;   int result =  0;
+
+            // Output a beautiful * line demacator.
+            System.Console.WriteLine("*************************************");
+
+            // Display Program title
+            System.Console.WriteLine("This is a simple Division program");
+
+            // Output a beautiful * line demacator.
+            System.Console.WriteLine("*************************************");
+
+            // Ask user for the first input
+            System.Console.Write("Please enter your first number : ");
+
+            // convert user string input to integer and store in firstnumber
+            firstnumber = int.Parse(Console.ReadLine());
+
+            // Ask user for the second input
+            System.Console.Write("Please enter your second number : ");
+
+            // convert user string input to integer and store in secondnumber
+            secondnumber = int.Parse(Console.ReadLine());
+
+           // Divide the firstnumber by the secondnumber and store in the result variable
+            result = firstnumber / secondnumber;
+
+            // Display an empty line
+            System.Console.WriteLine();
+
+            //Display Result
+            System.Console.WriteLine("After Division the result is " + result);
         }
     }
 }
