567906cb4be9e9040f52fc06939b6155dd3c7eb7
Kelly online <kellyonline2016@gmail.com>
2020-03-24 21:57:59 +0100

    Crated a new Division program task

--- csharp/Tasks/Multiplication/Program.cs
+++ csharp/Tasks/Multiplication/Program.cs
@@ -7,23 +7,16 @@ namespace Multiplication
         static void Main(string[] args)
         {
             // Declare and initialize the variables
-
-            int firstnumber = 0;
-            int secondnumber = 0;
-            int thirdnumber = 0;
-            int result =  0;
+            int firstnumber = 0;   int secondnumber = 0;   int thirdnumber = 0;   int result =  0;
 
             // Output a beautiful * line demacator.
             System.Console.WriteLine("*************************************");
-
 
             // Display Program title
-            System.Console.WriteLine("This is a simple Division program");
-
+            System.Console.WriteLine("This is a simple Multiplication program");
 
             // Output a beautiful * line demacator.
             System.Console.WriteLine("*************************************");
-
 
             // Ask user for the first input
             System.Console.Write("Please enter your first number : ");
@@ -34,20 +27,23 @@ namespace Multiplication
             // Ask user for the second input
             System.Console.Write("Please enter your second number : ");
 
-
-            // convert user string input to integer and store in firstnumber
+            // convert user string input to integer and store in secondnumber
             secondnumber = int.Parse(Console.ReadLine());
-            
-    
-            // Divide the firstnumber by the secondnumber and store in the result variable
-            result = firstnumber / secondnumber;
 
+            // Ask user for the third input
+            System.Console.Write("Please enter your third number : ");
+
+            // convert user string input to integer and store in thirdnumber
+            thirdnumber = int.Parse(Console.ReadLine());            
+    
+            // Multiply the firstnumber by the secondnumber, thirdnumber and store in the result variable
+            result = firstnumber * secondnumber * thirdnumber;
 
             // Display an empty line
             System.Console.WriteLine();
 
             //Display Result
-            System.Console.WriteLine("After Division the result is " + result);
+            System.Console.WriteLine("After Multiplication the result is " + result);
         }
     }
 }
