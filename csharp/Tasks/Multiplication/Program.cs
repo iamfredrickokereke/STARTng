89ad49bac5d73315243fae919fc7da27b0bccfaf
Kelly online <kellyonline2016@gmail.com>
2020-03-24 21:27:41 +0100

    new project created

---
+++ csharp/Tasks/Multiplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- csharp/Variables/Program.cs
+++ csharp/Variables/Program.cs
@@ -6,29 +6,46 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            // Declare and initialize the variables
+
             int firstnumber = 0;
             int secondnumber = 0;
             int result =  0;
 
+            // Output a beutiful * line demacator.
             System.Console.WriteLine("*************************************");
 
+
+            // Display Program title
             System.Console.WriteLine("This is a simple Division program");
 
-            System.Console.WriteLine();
 
+            // Output a beutiful * line demacator.
             System.Console.WriteLine("*************************************");
 
+
+            // Ask user for the first input
             System.Console.Write("Please enter your first number : ");
+
+            // convert user string input to integer and store in firstnumber
             firstnumber = int.Parse(Console.ReadLine());
 
-            
+            // Ask user for the second input
             System.Console.Write("Please enter your second number : ");
+
+
+            // convert user string input to integer and store in firstnumber
             secondnumber = int.Parse(Console.ReadLine());
             
+    
+            // Divide the firstnumber by the secondnumber and store in the result variable
             result = firstnumber / secondnumber;
 
+
+            // Display an empty line
             System.Console.WriteLine();
 
+            //Display Result
             System.Console.WriteLine("After Division the result is " + result);
 
         }
