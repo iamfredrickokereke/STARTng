58bfb390a891c531e5b179177eae925caa0608e5
Kelly online <kellyonline2016@gmail.com>
2020-03-25 02:06:27 +0100

    create a variable and array, also displayed its content

---
+++ JavaScript/Task1/index.js
@@ -0,0 +1,15 @@
+
+
+let name = "Kelly Eric"
+
+let courses = ["Html", "Css", "JavaScript", "Csharp", "NodeJs"]
+
+console.log(`my name is ${name}, and my courses are ${courses}`)
+
+for (let index = 0; index <= 200; index++) {
+    
+    if(index % 2 != 0 ){
+        console.log(index)
+    }
+    
+}
