25748d5885483778fc7989639e80050dd2f0935d
Kelly online <kellyonline2016@gmail.com>
2020-03-25 10:41:38 +0100

    added comments

--- JavaScript/Task1/index.js
+++ JavaScript/Task1/index.js
@@ -1,15 +1,26 @@
 
+// A variable to store name
 
 let name = "Kelly Eric";
 
+// An array to store list of courses
+
 let courses = ["Html", "Css", "JavaScript", "Csharp", "NodeJs"];
+
+// Display name
 
 console.log(`my name is ${name}, and my courses are :`);
 
+// Loop through Courses and display them
+
 courses.forEach((course, index) => console.log(`${index + 1} - ${course}`));
+
+// Display course length
 
 console.log(`My total course is ${courses.length}, and an odd number`);
 
+
+// Show Odd number of course length within 1-200 range
 
 for (let index = 0; index < 200; index++) {
     
