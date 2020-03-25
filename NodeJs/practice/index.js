044ee7b80501a21d72387cc8ed6ca20658f52e8d
Kelly online <kellyonline2016@gmail.com>
2020-03-25 19:57:03 +0100

    added new files

---
+++ NodeJs/practice/index.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var fs = require('fs');
+
+
+var app = http
+                .createServer( function(request,response){
+
+                    var readStream = fs.createReadStream('./index.html');
+                    response.writeHead(200, {"Content-type" : "text/html"});
+                    readStream.pipe(response);
+
+                     // if (request.url == '/')
+                    //response.end("Welcome to my site!");
+
+                    
+                    if(request.url == '/message'){
+
+                        response.end("File submitted");
+
+                        var data = request.body;
+
+                        request.on("data", function(data) {
+                                var decodedData = decodeURI(data)
+                                  .replace(/\+/, " ")
+                                  .replace("message=", "");
+                                fs.writeFile("./message.txt", decodedData, function(err) {
+                                  if (err) throw err;
+                                });
+                              });
+                    }
+                    
+                })
+                .listen(5000);
