040b83c0394db87a09eba41761efb8f00f104dbf
Kelly online <kellyonline2016@gmail.com>
2020-03-25 10:37:15 +0100

    updated the output

--- JavaScript/Task1/index.js
+++ JavaScript/Task1/index.js
@@ -1,16 +1,20 @@
 
 
-let name = "Kelly Eric"
+let name = "Kelly Eric";
 
-let courses = ["Html", "Css", "JavaScript", "Csharp", "NodeJs"]
+let courses = ["Html", "Css", "JavaScript", "Csharp", "NodeJs"];
 
-console.log(`my name is ${name}, and my courses are ${courses}`)
+console.log(`my name is ${name}, and my courses are :`);
+
+courses.forEach((course, index) => console.log(`${index + 1} - ${course}`));
+
+console.log(`My total course is ${courses.length}, and an odd number`);
 
 
-for (let index = 0; index <= 200; index++) {
+for (let index = 0; index < 200; index++) {
     
-    if(index % 2 != 0 ){
-        console.log(index)
+    if(index % 2 !== 0 ){
+        console.log(index);
     }
     
 }
