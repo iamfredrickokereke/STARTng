86ec19dd9463a460b6f8bec1d478f21a20c2f26c
Kelly online <kellyonline2016@gmail.com>
2020-03-30 13:43:48 +0100

    deprecation fixed and variable value used

--- NodeJs/MongoDB/index.js
+++ NodeJs/MongoDB/index.js
@@ -1,7 +1,8 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/kelly_eric";
+var Admin = "Kelly";
 MongoClient.connect(url,{useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
   if (err) throw err;
-  console.log("Database created!");
+  console.log("Database created! by " + Admin);
   db.close();
 });
