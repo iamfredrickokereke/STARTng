a60f572d3e5618d74d310d4a45c81324931d7af6
Kelly online <kellyonline2016@gmail.com>
2020-04-02 06:16:21 +0100

    modified files

--- NodeJs/MongoDB/index.js
+++ NodeJs/MongoDB/index.js
@@ -1,9 +1,13 @@
+
+// make client
 var MongoClient = require('mongodb').MongoClient;
-//Append the Database name you want to connect to (/slackname), to the base URL.
+
+//Append the Database name (/slackname), you want to connect to the base URL.
 var url = "mongodb://localhost:27017/kelly_eric";
+
 var Admin = "Kelly";
 
-// create a client to mongodb
+// make client connect to mongodb service/server
 MongoClient.connect( url, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
 
   // db object that points to the database "kelly_eric"  and store in dbo
@@ -20,20 +24,14 @@ MongoClient.connect( url, {useNewUrlParser: true, useUnifiedTopology: true}, fun
 
   // create 'users' collection in newdb database
   dbo.createCollection("interns", function(err, result) {
+
       if (err) throw err;
       console.log("Interns Collection has been created!");
-      // close the connection to db when you are done with it
+
+      // close the connection to db
       db.close();
   });
 
 });
 
 
-
-
- 
-// make client connect to mongo service
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    
-});
