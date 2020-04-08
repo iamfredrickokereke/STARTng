fc40b0a7041859042179fa09b52053061286ace3
Kelly online <kellyonline2016@gmail.com>
2020-04-08 22:28:50 +0100

    added content and comment

---
+++ JavaScript/Task2/index.js
@@ -0,0 +1,55 @@
+
+// Array of objects created
+const data=[
+    {principal: "2500", time: "1.8"},
+    {principal: "1000", time: "5"},
+    {principal: "3000", time: "1"},
+    {principal: "2000", time: "3"}
+];
+
+
+// Function that takes array with single argument
+
+function interestCalculator(data) {
+    let interestData = [];
+    for (let i = 0; i < data.length; i++) {
+        let principal = data[i].principal;
+        let time = data[i].time;
+        if (principal>=2500 && time>1 && time<3) {
+            let rate=3;
+            interest=(principal*rate*time)/100;
+
+            newData={principal:principal, rate:rate, time:time, interest:interest};
+            interestData.push(newData);                    
+        }
+        else if (principal>=2500 && time>=3) {
+            let rate=4;
+            interest=(principal*rate*time)/100;
+
+            newData={principal:principal, rate:rate, time:time, interest:interest};
+            interestData.push(newData);                    
+        }
+        else if (principal<2500 || time<=1) {
+            let rate=2;
+            interest=(principal*rate*time)/100;
+
+            newData={principal:principal, rate:rate, time:time, interest:interest};
+            interestData.push(newData); 
+        }
+        else{
+            let rate=1;
+            interest=(principal*rate*time)/100;
+
+            newData={principal:principal, rate:rate, time:time, interest:interest};
+            interestData.push(newData);
+        }
+    }
+
+    // logging interest data to console
+    console.log(interestData);
+
+    // returning the array value from the function
+    return interestData;
+    
+}
+interestCalculator(data);
