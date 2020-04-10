6fe6e0ca8eda18a2db80ffcd58a91c03790e117e
Kelly online <kellyonline2016@gmail.com>
2020-04-10 21:46:37 +0100

    added array

--- csharp/Task2/Program.cs
+++ csharp/Task2/Program.cs
@@ -6,7 +6,13 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            
+            int[] Tags = {1, 2, 3};
+            double[] prices = new double[3]; 
+            prices[0] = 1000;
+            prices[1] = 2000; 
+            prices[2] = 3000;
+
+            System.Console.WriteLine(prices[2]);
         }
     }
 }
