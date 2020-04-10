646955edc8b2fd91f7ad31ac037fdc93b29f74ff
Kelly online <kellyonline2016@gmail.com>
2020-04-10 21:57:43 +0100

    setting conditions

--- csharp/Task2/Program.cs
+++ csharp/Task2/Program.cs
@@ -6,13 +6,29 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            int[] Tags = {1, 2, 3};
+            int[] orders = {1, 2, 3};
             double[] prices = new double[3]; 
             prices[0] = 1000;
             prices[1] = 2000; 
             prices[2] = 3000;
 
-            System.Console.WriteLine(prices[2]);
+            System.Console.WriteLine("Welcome to Eric's Supermarket...");
+            System.Console.WriteLine("Enter an order number");
+
+            int orderNumber = int.Parse(Console.ReadLine());
+            bool isThereFood = false;
+            double price = 0;
+
+            for (int index = 0; index < orders.length; index++)
+            {
+                if (orderNumber == orders[index])
+                {
+                    price = prices[index];
+                    isThereFood = true;
+                    break;
+                }
+            }
+
         }
     }
 }
