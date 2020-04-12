8bf7daa3b3d67b9eceb9cc38761da83f4809a9ad
Kelly online <kellyonline2016@gmail.com>
2020-04-12 23:06:06 +0100

    add form body

--- NodeJs/NodeJs-Form-Handling/routes.js
+++ NodeJs/NodeJs-Form-Handling/routes.js
@@ -1,16 +1,28 @@
 
 const form = `
 
-    <html>
-
-    </html>
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title> Send us a Message</title>
+            </head>
+            <body>
+                <form action="/message" method="post">  
+                    <input type="text" name="input-box" id="">
+                    <input type="button" value="Send">
+                </form>
+            </body>
+            </html>
 
 `
 
 
 const requestHandler = (request, response) => {
 
-    
+        
     
     
     response.end();
