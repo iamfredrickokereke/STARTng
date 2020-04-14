d777ab6c5999c3084e107380fedc6659d93e2dd0
Kelly online <kellyonline2016@gmail.com>
2020-04-14 14:03:12 +0100

    creat a gradebook proj

---
+++ csharp/practice/Gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            args[0] = "fred";
+
+            Console.WriteLine($"Hello {args[0]}");
+        }
+    }
+}
