f621bc4d4dbd2028da2cecf07533ae10fc9a6242
Kelly online <kellyonline2016@gmail.com>
2020-04-22 22:01:00 +0100

    added string file

--- csharp/practice/string-interpolation/Program.cs
+++ csharp/practice/string-interpolation/Program.cs
@@ -6,10 +6,41 @@ namespace string_interpolation
     {
         static void Main(string[] args)
         {
-           string afriend = "Belrah";
-           string name = "fred";
+        //    string afriend = "Belrah";
+        //    string name = "fred";
 
-           System.Console.WriteLine($"Hello {afriend}, my name is {name}");
+        //    System.Console.WriteLine($"Hello {afrie(nd}, my name is {name}");
+
+        // int a = 5; int b = ++a; int c = b++;
+
+        // System.Console.WriteLine($"{a},{b},{c}");
+
+    //     int a = 5;
+
+    //     int b = a++;   //    !  its important to know that b = 5 but a = 6
+
+    //     string name = "fred";
+    //     string surname = "okereke";
+    //    System.Console.WriteLine(b.CompareTo(a));
+    //           System.Console.WriteLine();
+
+
+    //           System.Console.WriteLine(string.Compare(name, surname, true));
+    //            System.Console.WriteLine(string.Equals(name, surname));
+
+
+        // int x = 5;
+        // double y = x;
+
+        // double y = 10;
+        // int x = (int)y;
+
+        string b = "hello";
+
+
+
+        System.Console.WriteLine(b);
+
         }
     }
 }
