016575a175555bebd984ecd57c1b4506e79b9e87
Kelly online <kellyonline2016@gmail.com>
2020-05-06 15:01:21 +0100

    created a student class

---
+++ csharp/ClassesandObjects/Student.cs
@@ -0,0 +1,11 @@
+namespace ClassesandObjects
+{
+    public class  Student
+    {
+        public string Name {get; set;}
+
+        public string Course {get; set;}
+
+        public integer Score {get; set;}
+    }
+}
