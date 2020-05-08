e78a579744c1b23d814a2e9edf0529cfa9b1937a
Kelly online <kellyonline2016@gmail.com>
2020-05-08 17:45:39 +0100

    fix: fx index view

--- csharp/WebCalculator/Controllers/HomeController.cs
+++ csharp/WebCalculator/Controllers/HomeController.cs
@@ -18,10 +18,10 @@ namespace WebCalculator.Controllers
             _logger = logger;
         }
 
-        // public IActionResult Index()
-        // {
-        //     return View();
-        // }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
         // public IActionResult Privacy()
         // {
